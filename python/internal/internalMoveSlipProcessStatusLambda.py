790145b5a1aeae1a5bd4a82bb558f1617dc5600c
yoshimoto <motokiti2011@gmail.com>
2023-05-30 23:19:12 +0900

    530 work

--- python/internal/internalMoveSlipProcessStatusLambda.py
+++ python/internal/internalMoveSlipProcessStatusLambda.py
@@ -7,64 +7,77 @@ from boto3.dynamodb.conditions import Key
 # Dynamodbアクセスのためのオブジェクト取得
 dynamodb = boto3.resource('dynamodb')
 # 指定テーブルのアクセスオブジェクト取得
-userMyList = dynamodb.Table("userMyList")
+slipDetailInfo = dynamodb.Table("slipDetailInfo")
+salesServiceInfo = dynamodb.Table("salesServiceInfo")
+transactionSlip = dynamodb.Table("transactionSlip")
+completionSlip = dynamodb.Table("completionSlip")
 
 
-# 内部処理 引数に指定されたメッセージをマイリストTBLに登録する
+# 内部処理 引数に指定された操作で伝票の工程ステータス遷移処理を行う
 def lambda_handler(event, context) :
   print("Received event: " + json.dumps(event))
 
-  userList = event['userList']
-
+  # 操作ステータス
+  OperationStatus = event['OperationStatus']
   try:
-    # ユーザーリストの件数分引数で指定されたマイリストMsgに登録する
-    for user in userList :
-      sendMsg_query(event, user)
+    # 取引開始（ステータス0→1）
+    if OperationStatus == 'StartTrading' :
+      print('LABEL_1')
+      startTramsaction(event)
+    # 取引完了（ステータス1→2）
+    elif OperationStatus == 'EndOfTransaction' :
+      print('LABEL_2')
+      endTransaction(event)
+
   except Exception as e:
       print("Error Exception.")
       print(e)
 
-# ユーザーマイリストTBLメッセージ登録
-def sendMsg_query(event, userInfo):
 
-  now = datetime.now()
-  slipInfo = event['slipInfo']
-  category = event['category']
-  message = event['message']
-  requestInfo = event['requestInfo']
 
-  mechenicId = userInfo['mechenicId']
-  if mechenicId == None :
-    mechenicId = '0'
-
-  officeId = userInfo['officeId']
-  if officeId == None :
-    officeId = '0'
-
-  putResponse = userMyList.put_item(
-    Item={
-      'id' : str(uuid.uuid4()),
-      'userId' : userInfo['userId'],
-      'mechanicId' : mechenicId,
-      'officeId' : officeId,
-      'serviceType' : slipInfo['serviceType'],
-      'slipNo' : slipInfo['slipNo'],
-      'serviceTitle' : slipInfo['serviceTitle'],
-      'category' : category,
-      'message' : message,
-      'readDiv' : '0',
-      'messageDate' : now.strftime('%x %X'),
-      'messageOrQuastionId' : '' ,
-      'requestInfo' : requestInfo,
-      'deleteDiv' : '0',
-      'created' : now.strftime('%x %X'),
-      'updated' : now.strftime('%x %X')
-    }
-  )
+# 取引開始処理
+def startTramsaction(event) :
   
-  if putResponse['ResponseMetadata']['HTTPStatusCode'] != 200:
-    print(putResponse)
-    return putResponse
+  processStatus = '1'
+  slipNo = event['slipNo']
+  serviceType = event['serviceType']
 
+  # 対象伝票のステータスを更新する
+  slip = moveProcessStatus(slipNo, serviceType, processStatus)
+
+  # 取引伝票情報を作成する
+  postTransaction_query(slip)
+
+
+# 取引完了処理
+def endTransaction(event)
+
+  processStatus = '1'
+  slipNo = event['slipNo']
+  serviceType = event['serviceType']
+
+  # 対象伝票のステータスを更新する
+  slip = moveProcessStatus(slipNo, serviceType, processStatus)
+
+  # 取引伝票情報を論理削除する
+  deleteTransaction_query(slip)
+
+  # 完了済伝票情報を作成する
+  postCompletionSlip(slip)
+
+
+
+# 対象の伝票情報を取得
+def moveProcessStatus(slipNo, serviceType, processStatus):
+
+  if serviceType = '0' :
+    slipDitailStatusMove_query(slipNo, processStatus)
+  else :
+    salesServiceStatusMove_query(slipNo, processStatus)
+
+
+# 伝票情報のステータス操作
+def slipDitailStatusMove_query(slipNo, processStatus) :
+  
 
 
